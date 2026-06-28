9d066f641f90792705aaaffb3633ec97054bcb8b
0xN0WHERE <284443317+0xN0WHERE@users.noreply.github.com>
2026-06-28 20:39:45 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,1 +1,23 @@
+@echo off
+title Username sniper - Setup
 
+python --version >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Python is NOT installed!
+    echo Please install Python and add it to PATH.
+    pause
+    exit
+)
+
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Failed to install requirements!
+    pause
+    exit
+)
+
+python main.py
+
+pause
